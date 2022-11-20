6629fdeab423de5a73002fdbb0823f38060a6113
Nicolas Mouchel <nmo@octo.com>
2022-11-20 17:25:50 +0100

    ignore chrome extension

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,9 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(caches.open("all_cache").then((cache) => {
         return cache.match(event.request).then((cachedResponse) => {
             const fetchedResponse = fetch(event.request).then((networkResponse) => {
-                cache.put(event.request, networkResponse.clone());
+                if (!(event.request.url.indexOf('chr') === 0)) {
+                    cache.put(event.request, networkResponse.clone());
+                }
 
                 return networkResponse;
             });
